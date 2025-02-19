eb032a5f7f61d0b537e78fcbff3c15c2daad2df1
armanali485 <armanaligithub@gmail.com>
2025-02-20 01:16:47 +0530

    Nested Element Using React.createElement

--- App.js
+++ App.js
@@ -1,4 +1,30 @@
-let reactHeading= React.createElement("h1",{id:"heading"},"Hello from React Js");
+/*
+<div>   
+    <div>
+        <h1 id="heading">Hello</h1>
+    </div>
+</div>
+*/
+
+
+let reactHeading= React.createElement(
+    "div",
+    {},
+    React.createElement(
+        "div",
+        {},
+        [React.createElement(
+            "h1",
+            {id:"h1" ,style:{color:"blue"}},
+            "Hello"
+        ),
+        React.createElement(
+            "h2",
+            {id:"h2" ,style:{color:"orange"}},
+            "Hello"
+        )]
+    )
+);
 let root = ReactDOM.createRoot(document.querySelector("#root"));
     
 let x=root.render(reactHeading);
