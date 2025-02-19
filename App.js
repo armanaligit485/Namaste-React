557406ad6dbee933984d8672398c2a0cbb5fe64b
armanali485 <armanaligithub@gmail.com>
2025-02-20 00:53:37 +0530

    Added Js file and CSS file

---
+++ App.js
@@ -0,0 +1,4 @@
+let reactHeading= React.createElement("h1",{id:"heading"},"Hello from React Js");
+let root = ReactDOM.createRoot(document.querySelector("#root"));
+    
+let x=root.render(reactHeading);
