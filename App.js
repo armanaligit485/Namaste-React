8fe568f52de7884849a5d79b06d0f99777de0508
armanali485 <armanaligithub@gmail.com>
2025-02-22 11:08:33 +0530

    Parcel Installed

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ let reactHeading= React.createElement(
     React.createElement(
         "div",
         {},
-        [React.createElement(
+        React.createElement(
             "h1",
             {id:"h1" ,style:{color:"blue"}},
             "Hello"
@@ -22,9 +22,8 @@ let reactHeading= React.createElement(
             "h2",
             {id:"h2" ,style:{color:"orange"}},
             "Hello"
-        )]
+        )
     )
 );
 let root = ReactDOM.createRoot(document.querySelector("#root"));
-    
 let x=root.render(reactHeading);
